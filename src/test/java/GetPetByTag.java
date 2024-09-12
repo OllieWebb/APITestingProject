74d674c232487474a0f9b1af5c0d0074bb07ba53
OllieWebb <oliver.chester@hotmail.co.uk>
2024-09-12 14:07:32 +0100

    Adjusted the framework and added GET petByTag()

--- src/test/java/GetPetByTag.java
+++ src/test/java/GetPetByTag.java
@@ -1,2 +1,39 @@
+import config.AppConfig;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pojo.Pet;
+
 public class GetPetByTag {
+    private static Response response;
+    protected static final String BASE_URI = AppConfig.getBaseUri();
+    private static final String PET_PATH = "/findByTags?tags=string";
+    //private static final String TAG = "string";
+    private static Pet[] pets;
+
+    @BeforeAll
+    public static void beforeAll(){
+        response = RestAssured
+                .given()
+                .baseUri("http://petstore.swagger.io/v2")
+                .queryParam("tags", "string")  // Make sure "tags=string" is valid
+                .when()
+                .get("/pet/findByTags");
+
+        pets = response.as(Pet[].class);
+    }
+
+    @Test
+    public void validateResponse() {
+        if (response.getStatusCode() == 200) {
+            // Test may fail as the it is a public API so the data is constantly modified
+            MatcherAssert.assertThat(84, Matchers.is(pets.length));  // Example test
+        } else {
+            System.err.println("Test skipped due to non-200 response.");
+        }
+    }
 }
--- src/test/java/pojo/Pet.java
+++ src/test/java/pojo/Pet.java
@@ -5,7 +5,7 @@ import java.util.List;
 public record Pet(
         List<String> photoUrls,
         String name,
-        int id,
+        long id,
         Category category,
         List<TagsItem> tags,
         String status
